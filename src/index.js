ec6a89b3dbeca511520ae6f3778d02fa9dff2219
Ryan MacInnes <ryan.macinnes@gmail.com>
2020-03-20 11:14:34 -0700

    Adding simple js method to embed script.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
-console.log('index')
+
+export const embedTweeker = (businessId, scriptVersion = 'latest', options = {}) => {
+  setOptions(businessId, options)
+  injectScript(scriptVersion)
+}
+
+const setOptions = (businessId, options = {}) => {
+  return window.TweekerSettings = {
+    ...options,
+    businessId: businessId
+  }
+}
+
+const injectScript = (scriptVersion = 'latest') => {
+  const script = document.createElement('script')
+  script.async = true
+  script.src = `https://embed.tweeker.io/${scriptVersion}.js`
+
+  const headOrBody = document.head || document.body
+
+  if (!headOrBody) {
+    throw new Error(
+      'Expected document.body not to be null. tweeker embed.js requires a <body> element.'
+    );
+  }
+
+  headOrBody.appendChild(script);
+
+  return script
+}
