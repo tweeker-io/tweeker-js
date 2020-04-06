f523f367140a0b35a8b00e436754dcd970cb263e
Ryan MacInnes <ryan.macinnes@gmail.com>
2020-04-06 16:15:52 -0700

    Using rollup.

---
+++ rollup.config.js
@@ -0,0 +1,44 @@
+import babel from 'rollup-plugin-babel'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import pkg from './package.json'
+
+const INPUT_FILE_PATH = 'src/index.js';
+const OUTPUT_NAME = 'TweekerJs';
+
+const PLUGINS = [
+  babel({
+    exclude: 'node_modules/**',
+  }),
+  resolve({
+    browser: true,
+  }),
+  commonjs(),
+]
+
+const OUTPUT_DATA = [
+  {
+    file: pkg.browser,
+    format: 'umd',
+  },
+  {
+    file: pkg.main,
+    format: 'cjs',
+  },
+  {
+    file: pkg.module,
+    format: 'es',
+  },
+]
+
+const config = OUTPUT_DATA.map(({ file, format }) => ({
+  input: INPUT_FILE_PATH,
+  output: {
+    file,
+    format,
+    name: OUTPUT_NAME,
+  },
+  plugins: PLUGINS,
+}))
+
+export default config
