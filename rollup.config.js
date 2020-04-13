f30dfd38109d1454ea3b484e52a79b1c22376ab7
Ryan MacInnes <ryan.macinnes@gmail.com>
2020-04-13 12:37:57 -0700

    Removing commonjs.

--- rollup.config.js
+++ rollup.config.js
@@ -1,38 +1,18 @@
 import babel from 'rollup-plugin-babel'
 import resolve from '@rollup/plugin-node-resolve'
-import commonjs from '@rollup/plugin-commonjs'
 import pkg from './package.json'
 
-const INPUT_FILE_PATH = 'src/index.js';
-const OUTPUT_NAME = 'TweekerJs';
-
-const PLUGINS = [
-  babel({
-    exclude: 'node_modules/**',
-  }),
-  resolve(),
-  commonjs(),
-]
-
-const OUTPUT_DATA = [
-  {
-    file: pkg.main,
-    format: 'cjs',
-  },
-  {
-    file: pkg.module,
-    format: 'es',
-  },
-]
-
-const config = OUTPUT_DATA.map(({ file, format }) => ({
-  input: INPUT_FILE_PATH,
+export default {
+  input: 'src/index.js',
+  plugins: [
+    babel({
+      exclude: 'node_modules/**',
+    }),
+    resolve(),
+  ],
   output: {
-    file,
-    format,
-    name: OUTPUT_NAME,
+    file: pkg.main,
+    format: 'es',
+    name: 'TweekerJs',
   },
-  plugins: PLUGINS,
-}))
-
-export default config
+}
